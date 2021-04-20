3ac193d06e49719025ada8fd8a1bcfbb91f2fc59
mertrkaradeniz <17050111054@ybu.edu.tr>
2021-04-20 22:45:34 +0000

    Animation Added

--- app/src/main/java/com/mertrizakaradeniz/animalquiz/view/MainActivity.kt
+++ app/src/main/java/com/mertrizakaradeniz/animalquiz/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mertrizakaradeniz.animalquiz.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
@@ -9,6 +11,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.LottieAnimationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mertrizakaradeniz.animalquiz.R
 import com.mertrizakaradeniz.animalquiz.adapter.OptionsBoardAdapter
@@ -29,11 +32,13 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private lateinit var fabSound: FloatingActionButton
     private lateinit var tvQuestions: TextView
     private lateinit var rvOptionsBoard: RecyclerView
+    private lateinit var animationSuccess: LottieAnimationView
+    private lateinit var animationFail: LottieAnimationView
     private lateinit var adapter: OptionsBoardAdapter
     private lateinit var tts: TextToSpeech
 
     private var boardSize = BoardSize.LEVEL_1
-    private var Qposition = 0
+    private var qPosition = 0
     private var question = ArrayList<Animal>()
     private var cards = ArrayList<Question>()
     private var answer = -1
@@ -48,11 +53,12 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         fabSound = findViewById(R.id.fabSound)
         tvQuestions = findViewById(R.id.tvQuestion)
         rvOptionsBoard = findViewById(R.id.rvOptionsBoard)
+        animationSuccess = findViewById(R.id.animationSuccess)
+        animationFail = findViewById(R.id.animationFail)
         tts = TextToSpeech(this, this)
 
         setupQuestion()
         setupOptionsBoard()
-
     }
 
     override fun onInit(status: Int) {
@@ -61,7 +67,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.i(TAG, "The Language specified is not supported!")
             } else {
-                speakOut(cards[Qposition].questionText.toString())
+                speakOut(cards[qPosition].questionText.toString())
             }
         } else Log.i(TAG, "Initialization Failed!")
     }
@@ -72,6 +78,12 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         super.onDestroy()
     }
 
+    private fun animate(animationView : LottieAnimationView) {
+        animationView.visibility = View.VISIBLE
+        //animationView.setSpeed(1f)
+        animationView.playAnimation()
+    }
+
     private fun speakOut(text: String) {
         tts.speak(text, TextToSpeech.QUEUE_ADD, null, "")
     }
@@ -79,42 +91,55 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private fun setupOptionsBoard() {
         adapter = OptionsBoardAdapter(this, boardSize, object : CardClickListener {
             override fun onCardClicked(position: Int) {
-                if (Qposition == 2) {
-                    if (boardSize == BoardSize.LEVEL_1) boardSize = BoardSize.LEVEL_2
-                    else if (boardSize == BoardSize.LEVEL_2) boardSize = BoardSize.LEVEL_3
-                    else if (boardSize == BoardSize.LEVEL_3) boardSize = BoardSize.LEVEL_4
-                    else if (boardSize == BoardSize.LEVEL_4) boardSize = BoardSize.LEVEL_5
-                    else if (boardSize == BoardSize.LEVEL_5) Log.i(TAG, "YOU WON")
+                if (qPosition == 2 && position == answer) {
+                    when (boardSize) {
+                        BoardSize.LEVEL_1 -> boardSize = BoardSize.LEVEL_2
+                        BoardSize.LEVEL_2 -> boardSize = BoardSize.LEVEL_3
+                        BoardSize.LEVEL_3 -> boardSize = BoardSize.LEVEL_4
+                        BoardSize.LEVEL_4 -> boardSize = BoardSize.LEVEL_5
+                        BoardSize.LEVEL_5 -> Log.i(TAG, "YOU WON")
+                    }
                     score += 5
                     tvScore.setText("Score: $score")
                     speakOut("You got it!")
-                    setupQuestion()
-                    setupOptionsBoard()
-                    Thread.sleep(1000)
-                    Qposition = 0
-                    speakOut(cards[Qposition].questionText.toString())
-                }
-                else if (position == answer) {
-                    Qposition++
-                    Log.i(TAG, "$Qposition")
+                    animate(animationSuccess)
+                    Handler().postDelayed({
+                        setupQuestion()
+                        setupOptionsBoard()
+                        speakOut(cards[qPosition].questionText.toString())
+                        qPosition = 0
+                        animationSuccess.visibility = View.GONE
+                    }, 1500)
+
+                } else if (position == answer) {
+                    qPosition++
                     score += 5
                     tvScore.setText("Score: $score")
                     speakOut("You got it!")
-                    setupQuestion()
-                    adapter.notifyDataSetChanged()
-                    Thread.sleep(1000)
-                    speakOut(cards[Qposition].questionText.toString())
+                    animate(animationSuccess)
+                    Handler().postDelayed({
+                        setupQuestion()
+                        adapter.notifyDataSetChanged()
+                        speakOut(cards[qPosition].questionText.toString())
+                        animationSuccess.visibility = View.GONE
+                    }, 1500)
                     Log.i(TAG, "Answer is correct $answer")
                 } else if (position != answer) {
-                    Log.i(TAG, "Answer is not correct $answer")
                     speakOut("The answer is wrong!")
-                    Toast.makeText(this@MainActivity, "Game Over!!", Toast.LENGTH_SHORT).show()
+                    speakOut("Game Over")
+                    animate(animationFail)
+                    Handler().postDelayed({
+                        animationSuccess.visibility = View.GONE
+                        val intent = Intent(this@MainActivity, GameOverActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }, 2000)
                 }
             }
         })
         adapter.updateData(question)
         rvOptionsBoard.adapter = adapter
-        rvOptionsBoard.setHasFixedSize(true) //for efficient
+        rvOptionsBoard.setHasFixedSize(true)
         rvOptionsBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 
@@ -127,10 +152,10 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 cards.add(Q1Lvl1)
                 cards.add(Q2Lvl1)
                 cards.add(Q3Lvl1)
-                tvQuestions.setText(cards[Qposition].questionText)
-                question.add(cards[Qposition].animalOne!!)
-                question.add(cards[Qposition].animalTwo!!)
-                answer = cards[Qposition].correctAnswer!!
+                tvQuestions.setText(cards[qPosition].questionText)
+                question.add(cards[qPosition].animalOne!!)
+                question.add(cards[qPosition].animalTwo!!)
+                answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_2 -> {
                 supportActionBar!!.setTitle("Level 2")
@@ -139,11 +164,11 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 cards.add(Q1Lvl2)
                 cards.add(Q2Lvl2)
                 cards.add(Q3Lvl2)
-                tvQuestions.setText(cards[Qposition].questionText)
-                question.add(cards[Qposition].animalOne!!)
-                question.add(cards[Qposition].animalTwo!!)
-                question.add(cards[Qposition].animalThree!!)
-                answer = cards[Qposition].correctAnswer!!
+                tvQuestions.setText(cards[qPosition].questionText)
+                question.add(cards[qPosition].animalOne!!)
+                question.add(cards[qPosition].animalTwo!!)
+                question.add(cards[qPosition].animalThree!!)
+                answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_3 -> {
                 supportActionBar!!.setTitle("Level 3")
@@ -152,12 +177,12 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 cards.add(Q1Lvl3)
                 cards.add(Q2Lvl3)
                 cards.add(Q3Lvl3)
-                tvQuestions.setText(cards[Qposition].questionText)
-                question.add(cards[Qposition].animalOne!!)
-                question.add(cards[Qposition].animalTwo!!)
-                question.add(cards[Qposition].animalThree!!)
-                question.add(cards[Qposition].animalFour!!)
-                answer = cards[Qposition].correctAnswer!!
+                tvQuestions.setText(cards[qPosition].questionText)
+                question.add(cards[qPosition].animalOne!!)
+                question.add(cards[qPosition].animalTwo!!)
+                question.add(cards[qPosition].animalThree!!)
+                question.add(cards[qPosition].animalFour!!)
+                answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_4 -> {
                 supportActionBar!!.setTitle("Level 4")
@@ -166,14 +191,14 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 cards.add(Q1Lvl4)
                 cards.add(Q2Lvl4)
                 cards.add(Q3Lvl4)
-                tvQuestions.setText(cards[Qposition].questionText)
-                question.add(cards[Qposition].animalOne!!)
-                question.add(cards[Qposition].animalTwo!!)
-                question.add(cards[Qposition].animalThree!!)
-                question.add(cards[Qposition].animalFour!!)
-                question.add(cards[Qposition].animalFive!!)
-                question.add(cards[Qposition].animalSix!!)
-                answer = cards[Qposition].correctAnswer!!
+                tvQuestions.setText(cards[qPosition].questionText)
+                question.add(cards[qPosition].animalOne!!)
+                question.add(cards[qPosition].animalTwo!!)
+                question.add(cards[qPosition].animalThree!!)
+                question.add(cards[qPosition].animalFour!!)
+                question.add(cards[qPosition].animalFive!!)
+                question.add(cards[qPosition].animalSix!!)
+                answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_5 -> {
                 supportActionBar!!.setTitle("Level 5")
@@ -182,21 +207,21 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 cards.add(Q1Lvl5)
                 cards.add(Q2Lvl5)
                 cards.add(Q3Lvl5)
-                tvQuestions.setText(cards[Qposition].questionText)
-                question.add(cards[Qposition].animalOne!!)
-                question.add(cards[Qposition].animalTwo!!)
-                question.add(cards[Qposition].animalThree!!)
-                question.add(cards[Qposition].animalFour!!)
-                question.add(cards[Qposition].animalFive!!)
-                question.add(cards[Qposition].animalSix!!)
-                question.add(cards[Qposition].animalSeven!!)
-                question.add(cards[Qposition].animalEight!!)
-                answer = cards[Qposition].correctAnswer!!
+                tvQuestions.setText(cards[qPosition].questionText)
+                question.add(cards[qPosition].animalOne!!)
+                question.add(cards[qPosition].animalTwo!!)
+                question.add(cards[qPosition].animalThree!!)
+                question.add(cards[qPosition].animalFour!!)
+                question.add(cards[qPosition].animalFive!!)
+                question.add(cards[qPosition].animalSix!!)
+                question.add(cards[qPosition].animalSeven!!)
+                question.add(cards[qPosition].animalEight!!)
+                answer = cards[qPosition].correctAnswer!!
             }
         }
     }
 
     fun fab_OnClick(view: View) {
-        speakOut(cards[Qposition].questionText.toString())
+        speakOut(cards[qPosition].questionText.toString())
     }
 }
