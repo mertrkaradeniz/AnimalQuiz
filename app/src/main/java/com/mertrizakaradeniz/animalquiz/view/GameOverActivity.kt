907314f55cc815c7181db98f646cc8481a2fecb6
mertrkaradeniz <17050111054@ybu.edu.tr>
2021-05-02 02:03:57 +0000

    Splash screen added and Bug fixed

--- app/src/main/java/com/mertrizakaradeniz/animalquiz/view/GameOverActivity.kt
+++ app/src/main/java/com/mertrizakaradeniz/animalquiz/view/GameOverActivity.kt
@@ -1,13 +1,102 @@
 package com.mertrizakaradeniz.animalquiz.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.mertrizakaradeniz.animalquiz.R
 
+
 class GameOverActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "GameOverActivity"
+    }
+
+    private lateinit var animationConfetti: LottieAnimationView
+    private lateinit var animationFinish: LottieAnimationView
+    private lateinit var btnPlayAgain: Button
+    private lateinit var tvScore: TextView
+    private lateinit var tvResult: TextView
+    private lateinit var tvFirstScore: TextView
+    private lateinit var tvSecondScore: TextView
+    private lateinit var tvThirdScore: TextView
+
+    private var score = 0
+    private var firstScore = 0
+    private var secondScore = 0
+    private var thirdScore = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_game_over)
 
+        animationConfetti = findViewById(R.id.animationConfetti)
+        animationFinish = findViewById(R.id.animationFinish)
+        btnPlayAgain = findViewById(R.id.btnPlayAgain)
+        tvScore = findViewById(R.id.tvScore)
+        tvResult = findViewById(R.id.tvResult)
+        tvFirstScore = findViewById(R.id.tvFirstScore)
+        tvSecondScore = findViewById(R.id.tvSecondScore)
+        tvThirdScore = findViewById(R.id.tvThirdScore)
+
+        score = intent.getIntExtra("score", 0)
+        Log.d(TAG, "$score")
+        tvScore.text = "Score: $score"
+
+        if (score == 75) {
+            tvResult.text = "YOU WON"
+        }
+
+        setupSharedPreferences()
+
+        tvFirstScore.text = "1- $firstScore"
+        tvSecondScore.text = "2- $secondScore"
+        tvThirdScore.text = "3- $thirdScore"
+    }
+
+    private fun setupSharedPreferences() {
+        val preference = getSharedPreferences(
+            resources.getString(R.string.app_name),
+            Context.MODE_PRIVATE
+        )
+        val editor = preference.edit()
+        firstScore = preference.getInt("first", 0)
+        secondScore = preference.getInt("second", 0)
+        thirdScore = preference.getInt("third", 0)
+        if (thirdScore > firstScore) {
+            editor.putInt("first", thirdScore)
+            editor.putInt("third", firstScore)
+        } else if (secondScore > firstScore) {
+            editor.putInt("first", secondScore)
+            editor.putInt("second", firstScore)
+        } else if (thirdScore > secondScore) {
+            editor.putInt("second", thirdScore)
+            editor.putInt("third", secondScore)
+        }
+        val min = minOf(firstScore, secondScore, thirdScore)
+        if (score > min) {
+            if (firstScore == min) {
+                editor.putInt("first", score)
+            } else if (secondScore == min) {
+                editor.putInt("second", score)
+            } else if (thirdScore == min) {
+                editor.putInt("third", score)
+            }
+        }
+        editor.apply()
+    }
+
+    fun playAgain_OnClick(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("playAgain", true)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/mertrizakaradeniz/animalquiz/view/MainActivity.kt
+++ app/src/main/java/com/mertrizakaradeniz/animalquiz/view/MainActivity.kt
@@ -2,16 +2,16 @@ package com.mertrizakaradeniz.animalquiz.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.speech.tts.TextToSpeech
 import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.lottie.LottieAnimationView
@@ -23,6 +23,7 @@ import com.mertrizakaradeniz.animalquiz.models.BoardSize
 import com.mertrizakaradeniz.animalquiz.models.Question
 import com.mertrizakaradeniz.animalquiz.utils.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 
@@ -32,16 +33,20 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         private const val TAG = "MainActivity"
     }
 
-
+    private lateinit var tvTime: TextView
     private lateinit var tvScore: TextView
+    private lateinit var tvLevel: TextView
     private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     private lateinit var fabSound: FloatingActionButton
     private lateinit var tvQuestions: TextView
     private lateinit var rvOptionsBoard: RecyclerView
-    private lateinit var animationSuccess: LottieAnimationView
-    private lateinit var animationFail: LottieAnimationView
+    private lateinit var lottieAnimation: LottieAnimationView
+    private lateinit var loadingAnimation: LottieAnimationView
+    private lateinit var layoutRoot: ConstraintLayout
+    private lateinit var layoutAnimation: ConstraintLayout
     private lateinit var adapter: OptionsBoardAdapter
     private lateinit var tts: TextToSpeech
+    private lateinit var timer: CountDownTimer
 
     private var boardSize = BoardSize.LEVEL_1
     private var qPosition = 0
@@ -49,23 +54,45 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var cards = ArrayList<Question>()
     private var answer = -1
     private var score = 0
+    private var playAgain = false
+    private var duration: Long = TimeUnit.MINUTES.toMillis(2)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize the view elements
+        tvTime = findViewById(R.id.tvTime)
         tvScore = findViewById(R.id.tvScore)
+        tvLevel = findViewById(R.id.tvLevel)
         toolbar = findViewById(R.id.toolbar)
         fabSound = findViewById(R.id.fabSound)
         tvQuestions = findViewById(R.id.tvQuestion)
         rvOptionsBoard = findViewById(R.id.rvOptionsBoard)
-        animationSuccess = findViewById(R.id.animationSuccess)
-        animationFail = findViewById(R.id.animationFail)
+        lottieAnimation = findViewById(R.id.lottieAnimation)
+        loadingAnimation = findViewById(R.id.loadingAnimation)
+        layoutRoot = findViewById(R.id.layoutRoot)
+        layoutAnimation = findViewById(R.id.layoutAnimation)
         tts = TextToSpeech(this, this)
 
         setupQuestion()
         setupOptionsBoard()
+        setupTimer()
+
+        playAgain = intent.getBooleanExtra("playAgain", false)
+        if (!playAgain) {
+            Handler().postDelayed({
+                layoutRoot.isVisible = true
+                layoutAnimation.isVisible = false
+                loadingAnimation.pauseAnimation()
+            }, 4000)
+        } else {
+            Handler().postDelayed({
+            layoutRoot.isVisible = true
+            layoutAnimation.isVisible = false
+            loadingAnimation.pauseAnimation()
+            }, 2000)
+        }
     }
 
     override fun onInit(status: Int) {
@@ -74,7 +101,13 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.i(TAG, "The Language specified is not supported!")
             } else {
-                speakOut(cards[qPosition].questionText.toString())
+                if (!playAgain) {
+                    Handler().postDelayed({
+                        speakOut(cards[qPosition].questionText.toString())
+                    }, 3000)
+                } else {
+                    speakOut(cards[qPosition].questionText.toString())
+                }
             }
         } else Log.i(TAG, "Initialization Failed!")
     }
@@ -85,8 +118,30 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         super.onDestroy()
     }
 
-    private fun animate(animationView: LottieAnimationView) {
+    private fun setupTimer() {
+        timer = object : CountDownTimer(duration, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tvTime.text = "${(millisUntilFinished / 1000).toInt()} s"
+            }
+
+            override fun onFinish() {
+                speakOut("Time's up")
+                animate(lottieAnimation, R.raw.times_up)
+                Handler().postDelayed({
+                    lottieAnimation.visibility = View.GONE
+                    val intent = Intent(this@MainActivity, GameOverActivity::class.java)
+                    intent.putExtra("score", score)
+                    startActivity(intent)
+                    finish()
+                }, 2000)
+            }
+        }
+        timer.start()
+    }
+
+    private fun animate(animationView: LottieAnimationView, id: Int) {
         animationView.visibility = View.VISIBLE
+        animationView.setAnimation(id)
         animationView.playAnimation()
     }
 
@@ -103,39 +158,53 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                         BoardSize.LEVEL_2 -> boardSize = BoardSize.LEVEL_3
                         BoardSize.LEVEL_3 -> boardSize = BoardSize.LEVEL_4
                         BoardSize.LEVEL_4 -> boardSize = BoardSize.LEVEL_5
-                        BoardSize.LEVEL_5 -> Log.i(TAG, "YOU WON")
+                        BoardSize.LEVEL_5 -> {
+                            speakOut("YOU WON")
+                            animate(lottieAnimation, R.raw.congratulation)
+                            Handler().postDelayed({
+                                lottieAnimation.visibility = View.GONE
+                                val intent = Intent(this@MainActivity, GameOverActivity::class.java)
+                                intent.putExtra("score", score)
+                                startActivity(intent)
+                                finish()
+                            }, 2500)
+                            score += 5
+                            tvScore.setText("Score: $score")
+                            return
+                        }
                     }
                     score += 5
                     tvScore.setText("Score: $score")
                     speakOut("You got it!")
-                    animate(animationSuccess)
+                    animate(lottieAnimation, R.raw.successful)
                     Handler().postDelayed({
                         setupQuestion()
                         setupOptionsBoard()
                         speakOut(cards[qPosition].questionText.toString())
                         qPosition = 0
-                        animationSuccess.visibility = View.GONE
+                        lottieAnimation.visibility = View.GONE
                     }, 2000)
                 } else if (position == answer) {
                     qPosition++
                     score += 5
                     tvScore.setText("Score: $score")
                     speakOut("You got it!")
-                    animate(animationSuccess)
+                    animate(lottieAnimation, R.raw.successful)
                     Handler().postDelayed({
                         setupQuestion()
                         adapter.notifyDataSetChanged()
                         speakOut(cards[qPosition].questionText.toString())
-                        animationSuccess.visibility = View.GONE
+                        lottieAnimation.visibility = View.GONE
                     }, 2000)
                     Log.i(TAG, "Answer is correct $answer")
                 } else if (position != answer) {
                     speakOut("The answer is wrong!")
                     speakOut("Game Over")
-                    animate(animationFail)
+                    animate(lottieAnimation, R.raw.failed)
                     Handler().postDelayed({
-                        animationSuccess.visibility = View.GONE
+                        lottieAnimation.visibility = View.GONE
                         val intent = Intent(this@MainActivity, GameOverActivity::class.java)
+                        intent.putExtra("score", score)
                         startActivity(intent)
                         finish()
                     }, 2000)
@@ -151,7 +220,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private fun setupQuestion() {
         when (boardSize) {
             BoardSize.LEVEL_1 -> {
-                //supportActionBar!!.setTitle("Level 1")
+                tvLevel.text = "Level 1"
                 cards.clear()
                 question.clear()
                 cards.add(Q1Lvl1)
@@ -163,7 +232,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_2 -> {
-                //supportActionBar!!.setTitle("Level 2")
+                tvLevel.text = "Level 2"
                 cards.clear()
                 question.clear()
                 cards.add(Q1Lvl2)
@@ -176,7 +245,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_3 -> {
-                //supportActionBar!!.setTitle("Level 3")
+                tvLevel.text = "Level 3"
                 cards.clear()
                 question.clear()
                 cards.add(Q1Lvl3)
@@ -190,7 +259,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_4 -> {
-                //supportActionBar!!.setTitle("Level 4")
+                tvLevel.text = "Level 4"
                 cards.clear()
                 question.clear()
                 cards.add(Q1Lvl4)
@@ -206,7 +275,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 answer = cards[qPosition].correctAnswer!!
             }
             BoardSize.LEVEL_5 -> {
-                //supportActionBar!!.setTitle("Level 5")
+                tvLevel.text = "Level 5"
                 cards.clear()
                 question.clear()
                 cards.add(Q1Lvl5)
